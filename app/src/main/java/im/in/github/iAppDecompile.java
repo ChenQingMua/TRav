cf9de3a63d3e9b51961b8429a377396e27df8920
ChenQing <JisGreen@163.com>
2026-01-19 02:35:50 +0800

    Create iAppDecompile.java

---
+++ app/src/main/java/im/in/github/iAppDecompile.java
@@ -0,0 +1,17 @@
+package im.in.github;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class iAppDecompile extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+		setTitle("iApp逆向");
+        setContentView(R.layout.activity_iapp_decompile);
+        
+    }
+    
+}
